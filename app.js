8727e6e59a1201bc55f776857be76cbfd5d46263
jchck <hey@justinchick.com>
2020-09-10 09:54:31 -0400

    displaying dino data on btn click

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const DATA = './dino.json';
 
 function init() {
   (async () => {
+    /**
+     * build dinoArray with data fetched from external JSON
+     */
     const dinoArray = [];
     await fetch(DATA)
       .then(response => response.json())
@@ -11,9 +14,17 @@ function init() {
         })
       })
 
-      // console.log(dinoArray)
+      /**
+       * loop over dinoArray
+       * add img key to each entry
+       * set img value equal to the species name
+       */
+      dinoArray.forEach((entry) => {
+        let dinoImg = entry.species.toLowerCase()
+        entry.img = `images/${dinoImg}.png`
+      })
 
-        // Create Dino Constructor
+    // Animal constructor
     function Animal(species, weight, height, diet, where, when, fact, img) {
       this.species = species;
       this.weight = weight;
@@ -25,36 +36,23 @@ function init() {
       this.img = img;
     };
 
-
-    // Create Dino Objects
-    const dinoObjects = dinoArray.map(dino => new Animal(
-      dino.species,
-      dino.weight,
-      dino.height,
-      dino.diet,
-      dino.where,
-      dino.when,
-      dino.fact,
-      dino.img
-    ));
-
-    // console.log(dinoObjects)
-
-    // Create Human Object
+    /**
+     * use Animal constructor to create humanObject
+     */
     const humanObject = new Animal();
     // Use IIFE to get human data from form
       // data in dino.json is in lbs & inches
-      function humanData() {
-        (function(humanObject) {
-          humanObject.species = document.getElementById('name').value;
-          humanObject.weight = document.getElementById('weight').value;
-          humanObject.feet = document.getElementById('feet').value;
-          humanObject.inches = document.getElementById('inches').value;
-          humanObject.diet = document.getElementById('diet').value;
-          // humanObject.fact = ...
-          humanObject.img = 'images/human.png';
-        })(humanObject)
-      }
+    function humanData() {
+      (function(humanObject) {
+        humanObject.species = document.getElementById('name').value;
+        humanObject.weight = document.getElementById('weight').value;
+        humanObject.feet = document.getElementById('feet').value;
+        humanObject.inches = document.getElementById('inches').value;
+        humanObject.diet = document.getElementById('diet').value;
+        // humanObject.fact = ...
+        humanObject.img = 'images/human.png';
+      })(humanObject)
+    }
 
       // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches.
@@ -69,12 +67,29 @@ function init() {
 
     // Generate Tiles for each Dino in Array
     const tiles = () => {
-      for (let i = 0; i < dinoArray.length; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
-        gridItem.innerHTML = dinoArray[i].species;
 
+      for (let i = 0; i < dinoArray.length; i++) {
+
+        // create DOM elements
+        const gridItem = document.createElement('div');
+        const title = document.createElement('h3');
+        const img = document.createElement('img');
+        const fact = document.createElement('p');
+
+        // log dinos
+        // console.log(dinoArray)
+
+        // place DOM elements
         document.getElementById('grid').appendChild(gridItem);
+        gridItem.appendChild(title);
+        gridItem.appendChild(img);
+        gridItem.appendChild(fact);
+
+        // set content + attr of DOM elements
+        gridItem.className = 'grid-item';
+        title.innerHTML = dinoArray[i].species;
+        img.setAttribute('src', dinoArray[i].img);
+        fact.innerHTML = dinoArray[i].fact;
       }
     }
 
