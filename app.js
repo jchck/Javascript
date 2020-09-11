4aed826d2c41219fb3213750d4db51b231d3886c
jchck <hey@justinchick.com>
2020-09-11 09:38:59 -0400

    syntax: adding semi-colins

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function init() {
       .then(response => response.json())
       .then(response => {
         response.Dinos.map((dino) => {
-          dinoArray.push(dino)
+          dinoArray.push(dino);
         })
       })
 
@@ -20,8 +20,8 @@ function init() {
        * set img value equal to the species name
        */
       dinoArray.forEach((entry) => {
-        let dinoImg = entry.species.toLowerCase()
-        entry.img = `images/${dinoImg}.png`
+        let dinoImg = entry.species.toLowerCase();
+        entry.img = `images/${dinoImg}.png`;
       })
 
     // Animal constructor
@@ -34,7 +34,7 @@ function init() {
       this.when = when;
       this.fact = fact;
       this.img = img;
-    };
+    }
 
     /**
      * use Animal constructor to create humanObject
@@ -63,17 +63,17 @@ function init() {
 
       // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches.
-    const compareHeight = () => humanObject.height < dinoArray[0].height ? `${humanObject.species} is shorter than a ${dinoArray[0].species}!` : `${humanObject.species} is taller than a ${dinoArray[0].species}!`
+    const compareHeight = () => humanObject.height < dinoArray[0].height ? `${humanObject.species} is shorter than a ${dinoArray[0].species}!` : `${humanObject.species} is taller than a ${dinoArray[0].species}!`;
 
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
     // const compareWeight = humanObject.weight < dinoArray[2].weight ? `${humanObject.species}` is
     const compareWeight = () => {
       if (humanObject.weight < dinoArray[1].weight) {
-        return `${humanObject.species} is ${dinoArray[1].weight - humanObject.weight} pounds lighter than a ${dinoArray[1].species}`
+        return `${humanObject.species} is ${dinoArray[1].weight - humanObject.weight} pounds lighter than a ${dinoArray[1].species}`;
       }
       else {
-        return `${humanObject.species} is ${dinoArray[1].weight + humanObject.weight} pounds heavier than a ${dinoArray[1].species}`
+        return `${humanObject.species} is ${dinoArray[1].weight + humanObject.weight} pounds heavier than a ${dinoArray[1].species}`;
       }
     }
 
@@ -81,20 +81,20 @@ function init() {
     // NOTE: Weight in JSON file is in lbs, height in inches.
     const compareDiet = () => {
       if (humanObject.diet === 'Herbavor') {
-        return `${humanObject.species} has the same diet as a ${dinoArray[0].species} and a ${dinoArray[2].species}`
+        return `${humanObject.species} has the same diet as a ${dinoArray[0].species} and a ${dinoArray[2].species}`;
       }
       else if(humanObject.diet === 'Carnivor') {
-        return `${humanObject.species} has the same diet as a ${dinoArray[1].species}`
+        return `${humanObject.species} has the same diet as a ${dinoArray[1].species}`;
       }
       else {
-        return `${humanObject.species} is an Omnivor!`
+        return `${humanObject.species} is an Omnivor!`;
       }
     }
 
     // Generate Tiles for each Dino in Array
     const tiles = () => {
       // put human in middle of dinoArray
-      dinoArray.splice(4, 0, humanObject)
+      dinoArray.splice(4, 0, humanObject);
 
       for (let i = 0; i < dinoArray.length; i++) {
         // create DOM elements
@@ -128,5 +128,5 @@ document.getElementById('btn').addEventListener('click', () => {
   // Remove form from screen
   document.querySelector('form').style.display = 'none';
 
-  init()
+  init();
 })
